212b0a14d0d8aaa198778f410582b96011aeb974
Mariana Facorro <marianmfalicante@gmail.com>
2025-10-12 17:39:52 +0200

    feat: bugAsync exercise 5

--- bugAsync.js
+++ bugAsync.js
@@ -1,12 +1,13 @@
 // Este programa simula una llamada asincrónica para obtener un usuario
-function obtenerUsuario(id) {
-let usuario;
+function obtenerUsuario(id, cb) {
 setTimeout(() => {
-if (id === 1) {
-usuario = { id: 1, nombre: 'John Doe' };
-}
+    if (id === 1) {
+        const usuario = { id: 1, nombre: 'John Doe' };
+        cb(usuario);
+    }   
+
 }, 2000);
-return usuario;
 }
-const usuario = obtenerUsuario(1);
-console.log(usuario);
+const usuario = obtenerUsuario(1,(usuario) => {
+    console.log(usuario);
+});
